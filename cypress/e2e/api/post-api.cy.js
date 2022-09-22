640add6729d906608f6223b8129c297fea4573ed
Bruno OReilly Beringhs <bruno.beringhs@ambevtech.com.br>
2022-09-22 16:17:05 -0300

    Quinto commit

---
+++ cypress/e2e/api/post-api.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Fazer um post via API', () => {
+
+    let token
+
+    beforeEach(() => {
+        cy.token().then((tkn) => {
+            token = tkn
+        })
+    });
+
+    it.only('Deve criar um post via API', () => {
+        cy.request({
+            method: 'POST',
+            url: 'api/posts',
+            headers: {
+                Cookie: token
+            },
+            body:
+            {
+                "text": "Qualidade de Softwares"
+            }
+        }).then((response) => {
+            expect(response.status).to.equal(201)
+            expect(response.body.text).contains("Qualidade")
+        })
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,21 +13,21 @@ Cypress.Commands.add('criarPerfil', (status, cia, site, cidade, skills, github, 
   cy.get('#mui-component-select-status').click()
   cy.contains(status).click()
   cy.get('[data-test="profile-company"]').type(cia)
-    cy.get('[data-test="profile-webSite"]').type(site)
-    cy.get('[data-test="profile-location"]').type(cidade)
-    cy.get('[data-test="profile-skills"]').type(skills)
-    cy.get('[data-test="profile-gitHub"]').type(github)
-    cy.get('[data-test="profile-bio"]').type(minibio)
-    cy.get('[data-test="profile-submit"]').click()
+  cy.get('[data-test="profile-webSite"]').type(site)
+  cy.get('[data-test="profile-location"]').type(cidade)
+  cy.get('[data-test="profile-skills"]').type(skills)
+  cy.get('[data-test="profile-gitHub"]').type(github)
+  cy.get('[data-test="profile-bio"]').type(minibio)
+  cy.get('[data-test="profile-submit"]').click()
 })
 
 Cypress.Commands.add('cadastro', (nome, email, senha, confirmaSenha) => {
   cy.visit('cadastrar')
   cy.get('[data-test="register-name"] > .MuiInputBase-root > .MuiInputBase-input').type(nome)
-        cy.get('[data-test="register-email"] > .MuiInputBase-root > .MuiInputBase-input').type(email)
-        cy.get('[data-test="register-password"] > .MuiInputBase-root > .MuiInputBase-input').type(senha)
-        cy.get('[data-test="register-password2"] > .MuiInputBase-root > .MuiInputBase-input').type(confirmaSenha)
-        cy.get('[data-test="register-submit"]').click()
+  cy.get('[data-test="register-email"] > .MuiInputBase-root > .MuiInputBase-input').type(email)
+  cy.get('[data-test="register-password"] > .MuiInputBase-root > .MuiInputBase-input').type(senha)
+  cy.get('[data-test="register-password2"] > .MuiInputBase-root > .MuiInputBase-input').type(confirmaSenha)
+  cy.get('[data-test="register-submit"]').click()
 })
 
 import user from "../fixtures/multi-usuarios.json"
@@ -43,6 +43,5 @@ Cypress.Commands.add('token', () => {
   }).then((response) => {
     expect(response.status).to.equal(200) //opcional
     return response.body.jwt
-
   })
 })
